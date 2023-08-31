6c8e931887612b538adbd8b9701c5fb2b19e1934
Evan Kiser <kiser.evan1@gmail.com>
2023-08-30 22:13:31 -0700

    format results

--- src/components/FlightList.tsx
+++ src/components/FlightList.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
+import { Card, CardContent, Typography, Grid } from '@mui/material';
 
 export interface Flight {
   arrival_time: string,
   cabin_class: string,
-  carrier: string, 
-  departure_time: string, 
-  destination: string, 
-  dollar_cost: string, 
-  duration: number, 
-  origin: string, 
+  carrier: string,
+  departure_time: string,
+  destination: string,
+  dollar_cost: string,
+  duration: number,
+  origin: string,
   point_cost: number,
 }
 
@@ -17,15 +18,43 @@ interface Props {
 }
 
 const FlightList: FC<Props> = ({ flights }) => {
-  console.log(flights);
   return (
     <div>
       <h1>Available Flights</h1>
-      <ul>
+      <Grid container spacing={3}>
         {flights.map((flight, index) => (
-          <li key={index}>{`${flight.carrier}: ${flight.origin} -> ${flight.destination} in ${flight.cabin_class} for ${flight.point_cost} points and $${flight.dollar_cost}`}</li>
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card variant="outlined">
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  {flight.carrier}
+                </Typography>
+                <Typography>
+                  Origin: {flight.origin}
+                </Typography>
+                <Typography>
+                  Destination: {flight.destination}
+                </Typography>
+                <Typography>
+                  Cabin Class: {flight.cabin_class}
+                </Typography>
+                <Typography>
+                  Departure: {flight.departure_time}
+                </Typography>
+                <Typography>
+                  Arrival: {flight.arrival_time}
+                </Typography>
+                <Typography>
+                  Duration: {flight.duration} mins
+                </Typography>
+                <Typography>
+                  Cost: {flight.point_cost} points and ${flight.dollar_cost}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
         ))}
-      </ul>
+      </Grid>
     </div>
   );
 };
